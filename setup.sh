1994281c4d26b5e47c2e5d832e99ab4702c97ef4
anthanh <thanhan.lan@gmail.com>
2022-09-04 20:05:27 +0700

    change commit

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-export DATABASE_URL="postgresql://postgres:abc1@localhost:5432/postgres"
+export DATABASE_URL="postgresql://postgres:abc@localhost:5432/postgres"
 export EXCITED="true"
 echo "setup.sh script executed successfully!"
