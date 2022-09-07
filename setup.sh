e331e9e081df3ca155f9e63369c23afd62937703
anthanh <thanhan.lan@gmail.com>
2022-09-07 15:54:01 +0700

    update

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-export DATABASE_URL="postgresql://postgres:abc@localhost:5432/postgres"
+export DATABASE_URL="postgres://uzunflrcqmfcar:fd59339ba57966803822b89f4a82d1b63ca1da1173cf93e9967bf4f7b99659b1@ec2-52-72-99-110.compute-1.amazonaws.com:5432/dbbmjrmb4766q8"
 export EXCITED="true"
 echo "setup.sh script executed successfully!"
